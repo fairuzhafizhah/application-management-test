ba9ca6041a81b6c2dbe7c7a9cd47e809af7d406c
Nisrina Fairuz H.A.A <81574504+fairuzhafizhah@users.noreply.github.com>
2026-03-01 18:20:30 +0700

    Add index page

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+function goToLogin() {
+  navigateTo('/login')
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <p class="welcome">Welcome To</p>
+      <h1>
+        Application <br />
+        User <br />
+        Management
+      </h1>
+      <p class="created">Created by Hafizhah</p>
+    </div>
+
+    <button class="signin-btn" @click="goToLogin">
+      SIGN IN
+    </button>
+
+    <div class="circle top-left"></div>
+    <div class="circle bottom-right"></div>
+    <div class="dotted dotted-top"></div>
+    <div class="dotted dotted-bottom"></div>
+  </div>
+</template>
+
+<style>
+@import '@/assets/css/index.css';
+</style>
